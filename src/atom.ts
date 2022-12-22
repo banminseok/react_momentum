15bdd6a39d87d3b02d881f8d4b8ba408549bc74e
ban <halfmin@gmail.com>
2022-12-22 16:51:54 +0900

    localStorage username

--- src/atom.ts
+++ src/atom.ts
@@ -6,47 +6,43 @@ const { persistAtom } = recoilPersist({
   storage: localStorage,
 });
 
-// export enum Categories {
-//   "TO_DO" = "TO_DO",
-//   "DOING" = "DOING",
-//   "DONE" = "DONE",
+// export interface ICategories {
+//   text: string;
+//   id: number;
 // }
-export interface ICategories {
-  text: string;
-  id: number;
-}
 
 export const isDarkAtom = atom({
   key: "isDark",
   default: true,
 });
 
-export interface IToDo {
-  text: string;
-  id: number;
-  category: string;
-}
-export const categoryState = atom({
-  key: "category",
-  default: "TO_DO",
-});
-export const categoryListState = atom<ICategories[]>({
-  key: "categories",
-  default: [{ text: "TO_DO", id: 90001 }, { text: "DOING", id: 90002 }, { text: "DONE", id: 90003 }],
+// export interface IToDo {
+//   text: string;
+//   id: number;
+//   category: string;
+// }
+export const usernameState = atom({
+  key: "username",
+  default: "",
   effects_UNSTABLE: [persistAtom],
 });
+// export const categoryListState = atom<ICategories[]>({
+//   key: "categories",
+//   default: [{ text: "TO_DO", id: 90001 }, { text: "DOING", id: 90002 }, { text: "DONE", id: 90003 }],
+//   effects_UNSTABLE: [persistAtom],
+// });
 
-export const toDoSelector = selector({
-  key: "toDoSelector",
-  get: ({ get }) => {
-    const toDos = get(toDoState);
-    const category = get(categoryState);
-    return toDos.filter((toDo) => toDo.category === category);
-  },
-});
+// export const toDoSelector = selector({
+//   key: "toDoSelector",
+//   get: ({ get }) => {
+//     const toDos = get(toDoState);
+//     const category = get(categoryState);
+//     return toDos.filter((toDo) => toDo.category === category);
+//   },
+// });
 
-export const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
-  effects_UNSTABLE: [persistAtom],
-});
+// export const toDoState = atom<IToDo[]>({
+//   key: "toDo",
+//   default: [],
+//   effects_UNSTABLE: [persistAtom],
+// });
--- src/routes/Momentum.tsx
+++ src/routes/Momentum.tsx
@@ -1,8 +1,100 @@
+import styled from "styled-components";
+import { useForm } from "react-hook-form";
+import { useRecoilState } from "recoil";
+import { usernameState } from "../atom";
+
+const Wrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Container = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction : row;
+`;
+const Input = styled.input`
+  border: none;
+  -webkit-appearance: none;
+  -ms-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  background: #f2f2f2;
+  padding: 12px;
+  border-radius: 3px;
+  width: 350px;
+  font-size: 14px;
+  ime-mode: active;
+`;
+const Button = styled.button`
+  width: 20%;  
+  font-size: 20px;
+  border-radius: 5px;
+  padding : 8px 10px;
+  margin-left:2px;
+`;
+const ErrMsg = styled.div`
+  padding: 5px;
+  color : tomato;
+`;
+const TitleMyName = styled.div`
+  position: relative;
+  font-weight: 800;
+  font-size: 48px;
+  &:before {
+    content: '';
+    display: block;
+    position: absolute;
+    top: calc(50% + 2px);
+    left: -10px;
+    width: calc(100% + 20px);
+    height: 40%;
+    transform: translateY(-50%);
+
+    z-index: -1;
+    background-color: rgb(224, 185, 243);
+  }
+`
+
+
+interface IForm {
+  myName: string;
+}
+
 function Momentum() {
+  const [username, setUsername] = useRecoilState(usernameState);
+  const { register, handleSubmit, setValue, formState: { errors } } = useForm<IForm>();
+  const handleValid = ({ myName }: IForm) => {
+    setUsername(myName);
+    setValue("myName", "");
+  };
   return (
-    <>
-      <h1>Momentum</h1>
-    </>
+    <Wrapper >
+      {username === "" ? (
+        <>
+          <form onSubmit={handleSubmit(handleValid)}>
+            <Container>
+              <Input
+                {...register("myName", {
+                  required: "사용자 이름을 입력하세요.",
+                  maxLength: 15,
+
+                })}
+                placeholder="what is your name?" />
+              <Button>Add</Button>
+            </Container>
+          </form>
+          {errors.myName && <ErrMsg>{errors.myName.message}</ErrMsg>}
+        </>
+      ) : (
+        <TitleMyName>{username}</TitleMyName>
+      )}
+    </Wrapper>
   );
 }
 
